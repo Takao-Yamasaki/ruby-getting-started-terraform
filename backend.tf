395d1070418368d9a99515c772d9de49730b1a1a
Takao-Yamasaki <captain_kaeru@yahoo.co.jp>
2025-12-29 09:08:27 +0900

    first commit

---
+++ backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  cloud {
+    hostname = "otaco.app"
+    organization = "org_01KDKNC5SS218XZWY2AS75VBYA"    
+    workspaces {
+      name = "219b4729-efc7-4737-a815-a6b775c1686e"
+    }
+  }
+}

09bad0b295527b72c112298335f5488a82115086
Takao-Yamasaki <captain_kaeru@yahoo.co.jp>
2026-01-02 19:45:28 +0900

    add:lambdaのIAMロール作成

---
+++ lambda.tf
@@ -0,0 +1,49 @@
+# Lambda用のIAMロール
+resource "aws_iam_role" "lambda" {
+  name = "${var.project_name}-lambda-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+
+  tags = {
+    Name        = "${var.project_name}-lambda-role"
+    Environment = var.environment
+    Project     = var.project_name
+  }
+}
+
+# AWSLambdaBasicExecutionRoleをアタッチ
+resource "aws_iam_role_policy_attachment" "lambda_basic_execution" {
+  role       = aws_iam_role.lambda.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+# Lambda用のカスタムポリシー（RDSエクスポート用）
+resource "aws_iam_role_policy" "lambda" {
+  name = "${var.project_name}-lambda-policy"
+  role = aws_iam_role.lambda.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "iam:PassRole",
+          "rds:StartExportTask"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
+}

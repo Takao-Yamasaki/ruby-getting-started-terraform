611abd456b7e8022be6424172c36ed7b12b891ee
Takao-Yamasaki <captain_kaeru@yahoo.co.jp>
2026-02-01 18:18:46 +0900

    modify:パブリックサブネットに変更

--- network.tf
+++ network.tf
@@ -11,18 +11,30 @@ resource "aws_vpc" "main" {
   }
 }
 
-# Bastion用プライベートサブネット
+# Internet Gateway
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name        = "${var.project_name}-igw"
+    Environment = var.environment
+    Project     = var.project_name
+  }
+}
+
+# Bastion用パブリックサブネット
 resource "aws_subnet" "bastion" {
-  count             = length(var.bastion_subnet_cidr)
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = var.bastion_subnet_cidr[count.index]
-  availability_zone = "ap-northeast-1a"
+  count                   = length(var.bastion_subnet_cidr)
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = var.bastion_subnet_cidr[count.index]
+  availability_zone       = "ap-northeast-1a"
+  map_public_ip_on_launch = true
 
   tags = {
     Name        = "${var.project_name}-bastion-subnet-${count.index + 1}"
     Environment = var.environment
     Project     = var.project_name
-    Type        = "Private-Bastion"
+    Type        = "Public-Bastion"
   }
 }
 
@@ -41,7 +53,7 @@ resource "aws_subnet" "rds" {
   }
 }
 
-# Bastion用プライベートルートテーブル
+# Bastion用パブリックルートテーブル
 resource "aws_route_table" "bastion" {
   count  = length(var.bastion_subnet_cidr)
   vpc_id = aws_vpc.main.id
@@ -50,7 +62,7 @@ resource "aws_route_table" "bastion" {
     Name        = "${var.project_name}-bastion-rt-${count.index + 1}"
     Environment = var.environment
     Project     = var.project_name
-    Type        = "Private-Bastion"
+    Type        = "Public-Bastion"
   }
 }
 
@@ -65,6 +77,14 @@ resource "aws_route_table" "rds" {
     Project     = var.project_name
     Type        = "Private-RDS"
   }
+}
+
+# Bastionルートテーブルにインターネットへのルートを追加
+resource "aws_route" "bastion_internet" {
+  count                  = length(var.bastion_subnet_cidr)
+  route_table_id         = aws_route_table.bastion[count.index].id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.main.id
 }
 
 # Bastionサブネットとルートテーブルの関連付け
